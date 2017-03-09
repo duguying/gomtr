fe042e56ff5a2786435e5589e93710568ec0bd08
Rex Lee(李俊) <duguying2008@gmail.com>
2017-03-09 22:42:58 +0800

    travis

--- mtr_test.go
+++ mtr_test.go
@@ -13,17 +13,14 @@ func Test_Mtr(t *testing.T) {
 
 	time.Sleep(time.Second * 10)
 
-	i := 1
-	for {
-		mtr.Request("183.131.7.130", 10, func(response interface{}) {
-			//fmt.Println(response)
+	iplist := []string{"183.131.7.130","127.0.0.1","114.215.151.25","111.13.101.208"}
+
+	for i := 0; i < len(iplist); i++ {
+		mtr.Request(iplist[i], 10, func(response interface{}) {
 			task := response.(*MtrTask)
 			log.Blueln("[ID]", task.id)
 			fmt.Println(task.GetSummaryString())
 		})
-		i++
-		//time.Sleep(time.Second)
-		//break
 	}
 
 	for {
